85f75b42255fe3071e1335247c7ab921ce872d7a
lichang <dotb116393@163.com>
2025-09-30 16:07:37 +0800

    :new: 路由--优化处理、

--- src/stores/modules/router.ts
+++ src/stores/modules/router.ts
@@ -2,10 +2,12 @@ import { getUserMenusApi } from '@/api'
 import { defineStore } from 'pinia'
 import { useUserStore } from './user'
 import { generateAsyncRouters } from '@/utils/router'
+import router from '@/router'
+import type { RouteRecordRaw } from 'vue-router'
 
 /** 用户信息仓库 */
 export const useRouterStore = defineStore('router', () => {
-  /** 异步路由列表 */
+  /** 异步路由列表（只保留了部分字段） */
   const asyncRouters = ref<AppRouteRecordRaw[]>([])
 
   /** 获取异步路由 */
@@ -14,7 +16,10 @@ export const useRouterStore = defineStore('router', () => {
     if (!userInfo.companyId) return
     return getUserMenusApi(userInfo.companyId).then(({ data }) => {
       asyncRouters.value = generateAsyncRouters(data)
-      console.log(asyncRouters.value)
+      asyncRouters.value.forEach((route) => {
+        // addRoute方法只能接收只能断言成RouteRecordRaw类型，只能断言成RouteRecordRaw
+        router.addRoute(route as RouteRecordRaw)
+      })
     })
   }
 
--- src/types/router.d.ts
+++ src/types/router.d.ts
@@ -1,11 +1,8 @@
 import type { RouteRecordRaw } from 'vue-router'
 
 declare global {
-  /**
-   * 扩展的路由类型，保证 meta 一定存在并带上我们需要的字段
-   */
   interface AppRouteRecordRaw extends Omit<RouteRecordRaw, 'meta' | 'children'> {
-    meta: {
+    meta: RouteMeta & {
       title?: string
       order?: number
       icon?: string
